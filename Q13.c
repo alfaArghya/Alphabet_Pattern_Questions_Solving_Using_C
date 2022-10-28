887b7f7fa10af00e7420e8acd19ebc23553d4487
Arghya Das <95538110+alfaArghya@users.noreply.github.com>
2022-10-28 23:50:58 +0530

    Create Q13.c

---
+++ Q13.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(){
+  
+/*
+abcde
+ abcd
+  abc
+   ab
+    a
+*/
+
+    int n = 1; 
+    for(int i = 'e'; i >= 'a'; i--){
+        for(int j = 1; j <= n - 1; j++){
+            printf(" ");
+        }
+        for(int k = 'a'; k <= i; k++){
+            printf("%c",k);
+        }
+        printf("\n");
+        n++;
+    }
+  
+  return 0;
+}
